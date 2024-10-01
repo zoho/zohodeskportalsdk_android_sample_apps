66e5d7dd4678efdfe2bb3d98b41c295b4e2e1531
prakash-15296 <prakash.vel@zohocorp.com>
2024-10-01 12:46:53 +0530

    kotlin and java sample added separately for chat kit

---
+++ chat_kit_sample/src/main/java/com/zoho/deskportalsdk/demo/chat_kit_sample/MyApplication.java
@@ -0,0 +1,22 @@
+package com.zoho.deskportalsdk.demo.chat_kit_sample;
+
+import android.app.Application;
+import com.zoho.desk.asap.api.ZohoDeskPortalSDK;
+
+public class MyApplication extends Application {
+
+    public static ZohoDeskPortalSDK zohoDeskPortalSDKInstnace;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ZohoDeskPortalSDK.Logger.enableLogs();
+        zohoDeskPortalSDKInstnace = ZohoDeskPortalSDK.getInstance(this);
+        zohoDeskPortalSDKInstnace.initDesk(0 /*YOUR ORG ID*/,
+                "YOUR APP ID", ZohoDeskPortalSDK.DataCenter.US /*YOUR DATA CENTER*/);
+
+        /*
+         *  You would find the org Id, app Id and the Datacenter information from your Zoho Desk Portal -> ASAP -> Mobile section
+         * */
+    }
+}
